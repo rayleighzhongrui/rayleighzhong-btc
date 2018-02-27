f77be0eaad754de1e11284b95591c9acf25494d6
rayleighzhongrui <rayleighzhong@gmail.com>
2018-02-27 22:34:12 +0800

    add the alarm robot

--- btc/python/alarmbot.py
+++ btc/python/alarmbot.py
@@ -1,3 +1,11 @@
 import telegram
-bot = telegram.Bot(token = 489893423:AAHzD6yYYHN8ySds5khQMrMxNO_bEtoTRfE)
+from telegram.ext import Updater
+Token = "489893423:AAHzD6yYYHN8ySds5khQMrMxNO_bEtoTRfE"
+chat_id = 489172102
+bot = telegram.Bot(token = Token)
+updater = Updater(token=Token)
+dispatcher = updater.dispatcher
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_uid, text="i'm not a robot")
 
